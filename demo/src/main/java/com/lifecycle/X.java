63472bc64e50620c507002fab646307dd166d140
Shaad <141047163+Shaad98@users.noreply.github.com>
2024-08-19 10:23:43 +0530

    lifecycle method implementation by Interfaces

--- demo/src/main/java/com/lifecycle/X.java
+++ demo/src/main/java/com/lifecycle/X.java
@@ -1,6 +1,9 @@
 package com.lifecycle;
 
-public class X {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class X implements InitializingBean,DisposableBean {
     private int a;
 
     public int getA() {
@@ -30,5 +33,19 @@ public class X {
     public void dead()
     {
         System.out.println("We are inside destroy method...");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'destroy'");
+        System.out.println("We are calling destroy by Interface name DisposableBean");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'afterPropertiesSet'");
+        System.out.println("We are calling init by Interface name InitializingBean");
     }
 }
